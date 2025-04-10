c5977df1425f697e243ad3e1bbd9064ffa6b8272
JERKKY666 <elev8d.operations@gmail.com>
2025-04-09 19:54:56 -0500

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'JERKKY666'
+description 'Give Car / Delete Carplate Script'
+version '1.0.1'
+
+shared_script '@es_extended/imports.lua'
+
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    '@es_extended/locale.lua',
+    'server/main.lua'
+}
